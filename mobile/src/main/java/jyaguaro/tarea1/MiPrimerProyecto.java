4ed87b03e0b49343b6cbd53db08ea4634cf82b0b
jesus gerardo yaguaro oliveros <jesus gerardo yaguaro oliveros>
2016-08-21 19:47:49 -0400

    Mi Primera Entrega De Android

---
+++ mobile/src/main/java/jyaguaro/tarea1/MiPrimerProyecto.java
@@ -0,0 +1,13 @@
+package jyaguaro.tarea1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MiPrimerProyecto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mi_primer_proyecto);
+    }
+}
